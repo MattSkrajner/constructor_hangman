f3ab7e46569a949ba77bcfb25ab13171a6395639
MattSkrajner <matthew.skrajner@gmail.com>
2018-04-13 22:53:07 -0400

    upload letters.js

--- letters.js
+++ letters.js
@@ -1,13 +1,31 @@
+// Letter.js should not require any other files.
+
 // Letter.js: Contains a constructor, Letter. This constructor should be able to either display an underlying
 // character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the 
 // letter. That means the constructor should define:
-// A string value to store the underlying character for the letter
-// A boolean value that stores whether that letter has been guessed yet
-// A function that returns the underlying character if the letter has been guessed, or 
-// a placeholder (like an underscore) if the letter has not been guessed
-// A function that takes a character as an argument and checks it against the underlying character, 
-// updating the stored boolean value to true if it was guessed correctly
-// Letter.js should not require any other files.
+function Letter(letter, show ) {
+    // A string value to store the underlying character for the letter
+    this.letter = letter;
+    // A boolean value that stores whether that letter has been guessed yet
+    this.show = false;
+    // A function that returns the underlying character if the letter has been guessed, or 
+    // a placeholder (like an underscore) if the letter has not been guessed
+    // A function that takes a character as an argument and checks it against the underlying character, 
+    // updating the stored boolean value to true if it was guessed correctly
+    this.letterGuess = function() {
+        if(this.letter == " "){
+            this.show = true;
+            return " ";
+        }if (this.show === false){
+            return "_";
+        }else{
+            return this.letter;
+        }
+    };
+  };
+
+  module.exports = Letter;
+
 // HINT: Write Letter.js first and test it on its own before moving on, then do the same thing with Word.js
 // HINT: If you name your letter's display function toString, JavaScript will call that function automatically 
 // whenever casting that object to a string (check out this example: https://jsbin.com/facawetume/edit?js,console)
